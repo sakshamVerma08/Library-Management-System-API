7064daca84181f9c919f7e58284a850c1e0d65c7
Saksham Verma <sakshamverma1000@gmail.com>
2025-10-05 03:50:29 +0530

    Created user table in postgreSQL

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,11 +1,16 @@
 import { type Request, type Response } from "express";
+import * as z from "zod";
 
+const userSchema = z.object({
+    name: z.string().min(3),
+    
+})
 export const createUser = (req: Request, res: Response) => {
     const { email, password } = req.body as { email: string; password: string };
     
     if(!email || !password) return res.status(400).json({message:"Both fields are required for Signup"});
 
-    
+
 };
 
 
